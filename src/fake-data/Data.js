dcfcdfd059ad50827083032f3c24c4b0ee60fadb
Ramash3828 <ramash3828@gmail.com>
2022-03-25 21:16:24 +0600

    car-stores-project

---
+++ src/fake-data/Data.js
@@ -0,0 +1,75 @@
+const data = [
+    {
+        id: 1,
+        name: "Valbo",
+        img: "",
+        price: 5000,
+    },
+    {
+        id: 2,
+        name: "Valbo",
+        img: "",
+        price: 5000,
+    },
+    {
+        id: 3,
+        name: "Valbo",
+        img: "",
+        price: 5000,
+    },
+    {
+        id: 4,
+        name: "Valbo",
+        img: "",
+        price: 5000,
+    },
+    {
+        id: 5,
+        name: "Valbo",
+        img: "",
+        price: 5000,
+    },
+    {
+        id: 6,
+        name: "Valbo",
+        img: "",
+        price: 5000,
+    },
+    {
+        id: 7,
+        name: "Valbo",
+        img: "",
+        price: 5000,
+    },
+    {
+        id: 8,
+        name: "Valbo",
+        img: "",
+        price: 5000,
+    },
+    {
+        id: 9,
+        name: "Valbo",
+        img: "",
+        price: 5000,
+    },
+    {
+        id: 10,
+        name: "Valbo",
+        img: "",
+        price: 5000,
+    },
+    {
+        id: 11,
+        name: "Valbo",
+        img: "",
+        price: 5000,
+    },
+    {
+        id: 12,
+        name: "Valbo",
+        img: "",
+        price: 5000,
+    },
+];
+export default data;
